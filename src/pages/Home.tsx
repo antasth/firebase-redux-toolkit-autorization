460771beb5f999b53724ffc85b00a3afa42decad
antasth <antasth@mail.ru>
2023-12-11 23:27:49 +0300

    feat: add logout button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,20 @@
+import { useActions } from '@/hooks/useActions'
 import { useAuth } from '@/hooks/useAuth'
 import { Navigate } from 'react-router-dom'
 
 const Home = () => {
   const { isAuth } = useAuth()
 
+  const { removeUser } = useActions()
+
+  const handleLogout = () => {
+    removeUser()
+  }
+
   return isAuth ? (
     <div>
       <h1>Wellcome</h1>
+      <button onClick={handleLogout}>Logout</button>
     </div>
   ) : (
     <Navigate to={'/signin'} />
